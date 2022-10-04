92131f954d5db1acb113a431da2b1e88dde8cdbe
Diego Morelo <87763559+Dimores@users.noreply.github.com>
2022-10-04 11:05:36 -0300

    Add files via upload

--- ManipulandoArquivo/manipula.c
+++ ManipulandoArquivo/manipula.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <conio.h>
 
-    FILE *arquivo;
+FILE *arquivo; //Variável global para o arquivo
 
 //Função que abre um arquivo
-void fileOpen(){
+void fileOpenRead(){
     arquivo = fopen("teste.txt", "rt");
 
     if(arquivo == NULL){
@@ -14,13 +14,39 @@ void fileOpen(){
 
 }
 
+void fileOpenWrite(){
+    arquivo = fopen("teste.txt", "w");
+
+    if(arquivo == NULL){
+        printf("Nao foi possivel abrir o arquivo\n");
+        return;
+    }
+    
+}
+
 //Função que fecha um arquivo
 void fileClose(){
     arquivo = fclose(arquivo);
 }
 
+//Função que escreve uma palavra no arquivo
+void fileWrite(){
+    fileOpenWrite();
+
+    //fwrite(void *buffer,int numero_de_bytes,int count,FILE *fp);
+    char str[30] = "";  //Não consegui criar uma string com tamanho dinâmico
+    printf("Escreva no arquivo: ");
+    scanf("%s", &str);
+
+    fwrite(str, 1, sizeof(str), arquivo);
+
+    fileClose();
+}
+
 //Função que imprime o arquivo
 void filePrint(){
+    fileOpenRead();
+
     char *linha[100];  //Ponteiro de char para linhas
     char *result;  //Ponteiro de char para resultados
     int i;       //Incremento para o while
@@ -42,16 +68,14 @@ void filePrint(){
 
 //Função que chama as outras funções do programa
 void fileOperation(){
-    fileOpen();
+    fileWrite();
     filePrint();
-    fileClose();
-}
 
+}
 
 int main(){
 
     fileOperation();
-
 
     return 0;
 }
